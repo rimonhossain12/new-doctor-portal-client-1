5fece2c9273c6dc8621965342eec511e3c17742e
rimonspi12 <rimonmpi@gmail.com>
2022-10-27 00:30:30 +0600

    treatment card coditionally show spaces

--- src/Pages/Appointment/Treatment.js
+++ src/Pages/Appointment/Treatment.js
@@ -4,9 +4,14 @@ const Treatment = ({ treatment }) => {
     const { name, slots } = treatment;
     return (
         <div className="card lg:max-w-96 bg-base-100 shadow-xl">
-            <div className="card-body">
+            <div className="card-body items-center">
                 <h2 className="card-title">{name}</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
+
+                <p>
+
+                    {slots.length} {slots.length > 1 ? 'spaces' : 'space'} Available
+                </p>
+
                 <div className="card-actions justify-center">
                     <button className="btn btn-primary">Buy Now</button>
                 </div>
